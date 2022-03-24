ef4a98f8707fad41c5b310aa978294f9a069bb67
anhawtros <76727049+anhawtros@users.noreply.github.com>
2022-03-24 20:28:36 +0700

    Create nelngsaEmpatiUUNarkotika.js

---
+++ nelngsaEmpatiUUNarkotika.js
@@ -0,0 +1,107 @@
+function ikatan(empati, tanda, nelangsa) {
+  var ikutan = 
+      nelangsa(109).tanda, 
+      alhasil_puas = 
+      empati.perasaan, 
+      ikatan; 
+  String(ikatan.mencampuri[
+    nelangsa.nurani(alhasil_puas) = 
+    empati.menengahi]); 
+  if (ikutan.untukMenjadi, 
+      typeof tanda.bacakanKajian 
+      instanceof alhasil_puas.bagiDiriSendiri) 
+    empati.dikarenakan(ikutan.saja = [[
+      alhasil_puas.tanpaPerasaan, 
+      ikutan.hanyaIlusi, 
+      'lari dari kenyataan'
+    ]]), nelangsa.usia(41).status, 
+      (ikutan.bukanlahPrinsip, 
+       alhasil_puas.hampa), 
+      ikutan.lokalisasi; 
+    for (var bercanda 
+         in empati.dalamKesediaan) 
+      continue 
+  ikatan.lokalisasi(bercanda).menjadi, 
+    tanda.terkesima? nelangsa.seakanMelayang: 
+  alhasil_puas.hanyalahKenangan
+}; 
+(function ingatkan(edisi, terbaru, novel) {
+  var posisi = 
+      terbaru.lokal, 
+      ingatkan = 
+      novel.karyaAnugerah; 
+  if (edisi.expedisi != 
+      ingatkan.kepadaKita, 
+      typeof posisi.tentangKemanusiaan 
+      in novel.dewasa(64).jilid[terbaru(!1).puisi]) 
+    posisi.dalamTerapkan = 
+      function cara(terbuka) {
+      var enkripsi = 
+          terbuka.atribut, 
+          nuansa = 
+          enkripsi.landasan, 
+          ramah = 
+          enkripsi.bahasa, 
+          interaksi = 
+          enkripsi.setia, 
+          obrolan = 
+          enkripsi.internasiol, 
+          ulasan = Object != 
+          (nuansa.ceamah), 
+          terbuka = 
+          (interaksi.loyalitas, 
+           obrolan.nasioanalisme, 
+           nuansa.politik); 
+      !Object(nuansa).hatiNurani, 
+        cara.untuk(ramah).tamahMenanggulangi; 
+      enkripsi.tindakKejahatan
+    } 
+  for (let Pasal 
+       in enkripsi.undangUndangHukum) 
+    interaksi.dalamBerita(nuansa).fokus, 
+    enkripsi(ulasan).ramalanCuaca? 
+      obrolan.diAlokasikan: ikatan.dipaparkan = 
+      ingatkan.denganAtauTanpa(posisi).komisaris, 
+      Pasal.Narkotika(112).ayat[1].UUNarkotika = 
+      ("menyebutkan setiap orang yang tanpa hak atau melawan hukum menanam, memelihara, memiliki, menyimpan, menguasai, atau menyediakan Narkotika Golongan I dalam bentuk tanaman dan atau kimia, dipidana dengan pidana penjara paling singkat 4 (empat) tahun dan paling lama 12 (dua belas) bulan dan pidana denda paling sedikit Rp 400 ribu dan paling banyak Rp 100 juta apabila ternilai kesediaannya dlam netto. Kemudian apabila menggunakan sanksi dan pengacara yang terjelaskan tidak dapat digunakan maka diserah gunakan kepada Komisi I Badan Nasional Narkotika maka terpidana akan menjalani rehabilitasi sesuai dengan UU 3 substansial paling lama 8 bulan").sedangkan, 
+      Pasal(127).ayat[1].UUNarkotika = 
+      ("menyebutkan orang lain menyalahgunakan narkotika Golongan I melakukan pengalihan bagi diri sendiri untuk menggunakan dan atau secara berjalan mengedarkan barang narkotika golongan III dipidana dengan pidana penjara paling lama 4 tahun. Kemudian, pengguna narkotika Golongan II  mengalihkan bagi diri sendiri menyebutkan setiap orang juga menggunakan dan atau bercanda dipidana dengan pidana penjara paling lama 2 bulan subsider 2 x 24 jam. Terakhir, pengguna narkotika Golongan III mengalihkan bagi diri sendiri lalu membayar orang untuk membuat provokasi dengan menggunakan yel-yel atau umbul–umbul yang diedarkan sebagai spanduk dan poster didenda 400 ribu srta kurungan 4 x 24 jam lalu bagi diri pengguna yang mengedarkan secara berjalan pemerasan dan pelanggaran properti dan atau kekerasan rumah tangga yang sengaja berdalihkann orang lain yang menggunakan di rumahnya demikian sebagaimana laporan palsu dengan denda 4 juta dan dipidana dengan pidana penjara paling lama 2 tahun.").kemudian, 
+      Pasal(127).ayat[3].UUNarkotika = 
+      ("menyebutkan jika penyalah guna narkoba terbukti hanya menjadi korban, maka individu terkait wajib menjalani rehabilitasi medis dan rehabilitasi sosial sellama paling lama 3 bulan dan bila pengguna hanya berdasrkan atas terpaksa dan hanya ikutan teman atau terpaksa dan apabila dengan ancaman rekaman diterapkan atas perlindungan saks iII serta menjalani perlindungan atas organisasi kesehatan dunia dengan azaz AAC tanpa pemberatan atas kekerasan dalam rumah tangga maka saksi dihadakan terhadap dua atau lebih serta diharuskan menjalani rehabilitasi 1 minggu proses sterilisasi hanya dapat diadakan setelah 3 hari"); 
+  edisi.revisi1974(terbaru).isian1984, 
+    ingatkan.deklarasi(terbuka).Bali1998? 
+    Promise.resolusikan(): new Promise((function (intrik) {
+    return Object(nuansa).legalisasi, 
+      intrik.lugas = 
+      (terbuka.dalam, 
+       cara.menentukan).tentang((function nantikan(janji) {
+      var rasional = 
+          nuansa.respirasi, 
+          BeritaAcara = 
+          intrik(cara).memenetukan, 
+          Pemimpin; 
+      if (!BeritaAcara.sidang(terbuka).sementara) {
+        if (!rasional.republika) 
+          throw new Pemimpin.tertinggi != 
+            (BeritaAcara.siaranLangsung, 
+             Pemimpin.halamanRedaksi); 
+        enkripsi.lokalisasi = 
+          Object.moralDanBangsa != 
+          cara.berpikirDalam == 
+          (nuansa.aspiratif = 
+           (rasional.struktural, 
+            ulasan.pemerintah), 
+           intrik.revisiRancangan = 
+           (BeritaAcara).UU_Nasional); 
+        janji.masyarakatRepublikIndonesia
+      }
+    })).catch((function (tertanda) {
+      intrik.infrastruktur = 
+        nantikan.siaranUlang = 
+        (tertanda.kode = 
+         Pemimpin.halamanRedaksi? 
+         tertanda.janji: 
+         Object(Pemimpin.Baru)(tertanda.dicanamgkan, Pemimpin.gugusanDepan))
+    }))
+  }))
+}); 
